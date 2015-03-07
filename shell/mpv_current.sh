b27bed3f3c0af429635a8d5cb48dd519ed937c67
root <root@pentabot>
2015-03-07 22:55:14 +0100

    mpv streaming

--- shell/mpv_current.sh
+++ shell/mpv_current.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-ps -ax | grep "[m]pv" | awk '{print $11}' > /tmp/mpv_current.log
+#/ ps -ax | grep "[m]pv" | awk '{print $7,$11}' > /tmp/mpv_current.log
+
+ps -ax | grep "cider" | awk '{print $7,$11}' > /tmp/mpv_current.log
+ps -ax | grep "zaubert" | awk '{print $7,$11}' >> /tmp/mpv_current.log
+
+sed -i 's/.hq.c3d2.de//g' /tmp/mpv_current.log
 sed -i '/^$/d' /tmp/mpv_current.log
 
 # EOF
