3e6080d71012553a776259c685002c360820b374
root <root@pentabot>
2015-03-19 21:33:30 +0100

    checks

--- shell/check_wetu.sh
+++ shell/check_wetu.sh
@@ -4,12 +4,20 @@
 TMPFILE=/tmp/pentabot_check_wetu.log
 TMPFILE1=/tmp/pentabot_check_wetu1.log
 
+# check - dns: ns.c3d2.de
+DNS=$(nc -vz -w 2 -t ns.c3d2.de 53 2>&1 > /dev/null | grep -c "succeeded")
+if [ "$DNS" = "1" ]; then
+   echo "Dns(tcp)=OK" > "$TMPFILE"
+else
+   echo "Dns(tcp)=FAILED" > "$TMPFILE"
+fi
+
 # check - http: c3d2.de
 WEB=$(/usr/bin/curl -m3 -I http://www.c3d2.de/ 2>&1 | grep 'HTTP/' | grep -c "OK")
 if [ "$WEB" = "1" ]; then
-   echo "Web=OK" > "$TMPFILE"
+   echo "Web=OK" >> "$TMPFILE"
 else
-   echo "Web=FAILED" > "$TMPFILE"
+   echo "Web=FAILED" >> "$TMPFILE"
 fi
 
 # check - https: wiki.c3d2.de
@@ -37,7 +45,7 @@ else
 fi
 
 # check - smtp: mail.c3d2.de
-SMTP=$(nc -vz -w 1 -t mail.c3d2.de 25 2>&1 > /dev/null | grep -c "succeeded")
+SMTP=$(nc -vz -w 2 -t mail.c3d2.de 25 2>&1 > /dev/null | grep -c "succeeded")
 if [ "$SMTP" = "1" ]; then
    echo "Mail(smtp)=OK" >> "$TMPFILE"
 else
@@ -45,7 +53,7 @@ else
 fi
 
 # check - imap: mail.c3d2.de
-IMAP=$(nc -vz -w 1 -t mail.c3d2.de 143 2>&1 > /dev/null | grep -c "succeeded")
+IMAP=$(nc -vz -w 2 -t mail.c3d2.de 143 2>&1 > /dev/null | grep -c "succeeded")
 if [ "$IMAP" = "1" ]; then
    echo "Mail(imap)=OK" >> "$TMPFILE"
 else
@@ -53,7 +61,7 @@ else
 fi
 
 # check - xmpp: jabber.c3d2.de
-JABBER=$(nc -vz -w 1 -t jabber.c3d2.de 5269 2>&1 > /dev/null | grep -c "succeeded")
+JABBER=$(nc -vz -w 2 -t jabber.c3d2.de 5269 2>&1 > /dev/null | grep -c "succeeded")
 if [ "$JABBER" = "1" ]; then
    echo "Jabber=OK" >> "$TMPFILE"
 else
